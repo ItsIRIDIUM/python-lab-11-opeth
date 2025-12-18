7ff6a6b14bddffb44217e533a8f649aafe2b019c
╨е╨╕╨╝╨╡╤А╨░ ╨Р╨╜╤В╨╛╨╜ ╨б╨╡╤А╨│╤Ц╨╣╨╛╨▓╨╕╤З <vt241_khas@student.ztu.edu.ua>
2025-12-18 21:47:44 +0200

    Initial commit

--- app.py
+++ app.py
@@ -127,6 +127,8 @@ def delete_album(album_id):
     db.session.commit()
     return redirect(url_for('index'))
 
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
